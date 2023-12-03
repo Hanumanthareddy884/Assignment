bdec8e46a34c26486ff1b3b3f2296b762aac0673
Hanumanthareddy <nagarajh098@gmail.com>
2023-12-03 23:44:48 +0530

    Assignment Completed

--- csv_upload/main.py
+++ csv_upload/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI, Depends,File, UploadFile, Request
-from pydantic import BaseModel
-from . import schemas,models
+from . import models
 from .database import engine,SessionLocal
 from sqlalchemy.orm import Session
 import pandas as pd
-from io import StringIO,BytesIO
+from io import BytesIO
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-
+from fastapi.responses import HTMLResponse,RedirectResponse
+import uvicorn
 
 
 app= FastAPI()
@@ -28,20 +27,21 @@ async def all(request:Request,db:Session = Depends(get_db)):
     users = db.query(models.Users).all()
     return templates.TemplateResponse("index.html",{"request":request, "title":"MagicPitch LLC","users":users})
 
-@app.post('/register',status_code=201)
-def add(request:schemas.User,db:Session = Depends(get_db)):
-    new_user= models.Users(name = request.name,age = request.age)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
 @app.post("/upload")
-def upload(file: UploadFile = File(...),db:Session = Depends(get_db)):
+def uploads(file: UploadFile = File(...),db:Session = Depends(get_db)):
     contents = file.file.read()
     buffer = BytesIO(contents)
     df = pd.read_csv(buffer)
+    data = df.to_dict(orient="records")
     buffer.close()
     file.file.close()
+    for rows in data:
+        new_user= models.Users(name = rows['Name'],age = rows['Age'])
+        db.add(new_user)
 
-    return df.to_dict(orient='records')
+    db.commit()
+    db.refresh(new_user)
+    return RedirectResponse(url="/", status_code=302)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
